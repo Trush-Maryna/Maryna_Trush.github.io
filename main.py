5f5daa201547c59d03745564363e95acc5d774a7
Maryna_Trush <149105658+Trush-Maryna@users.noreply.github.com>
2024-04-19 01:16:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 
 from keyboards import start_keyboard, categories_keyboard, medicines_keyboard, antibiotiki_app_button
-from config import BOT_TOKEN, PAYMENTS_TOKEN, ADMIN_ID
+from config import BOT_TOKEN, PAYMENTS_TOKEN, CHANNEL_ID
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -34,6 +34,40 @@ async def process_antibiotics(callback_query: CallbackQuery):
 
 
 @dp.message_handler(content_types=ContentType.WEB_APP_DATA)
+async def process_web_app_data(message: Message):
+    data = json.loads(message.web_app_data.data)
+    if data['type'] == 'order_info':
+        order_details = data['data']
+        total_price = data['totalPrice']
+        customer_info = data['customerInfo']
+
+        response_message = f"Дякуємо {customer_info['fullName']}! \nЗамовлення прийнято\nОсь ваш чек:\n"
+        for item in order_details:
+            response_message += f"{item['name']}, кількість: {item['quantity']}, ціна: {item['totalPrice']} грн.\n"
+        response_message += f"Загальна ціна: {total_price} грн.\n"
+        response_message += f"Деталі доставки:\n"
+        response_message += f"Номер телефону: {customer_info['phoneNumber']}\n"
+        response_message += f"Область: {customer_info['region']}\n"
+        response_message += f"Місто: {customer_info['city']}\n"
+        response_message += f"Відділення НП: {customer_info['office']}\n"
+        await message.answer(response_message)
+
+        channel_id = CHANNEL_ID
+        response_message = f"Нове замовлення від {customer_info['fullName']}!\n"
+        for item in order_details:
+            response_message += f"{item['name']}, кількість: {item['quantity']}, ціна: {item['totalPrice']} грн.\n"
+        response_message += f"Загальна ціна: {total_price} грн.\n"
+        response_message += f"Деталі доставки:\n"
+        response_message += f"ПІБ: {customer_info['fullName']}\n"
+        response_message += f"Номер телефону: {customer_info['phoneNumber']}\n"
+        response_message += f"Область: {customer_info['region']}\n"
+        response_message += f"Місто: {customer_info['city']}\n"
+        response_message += f"Відділення НП: {customer_info['office']}\n"
+        await bot.send_message(channel_id, response_message)
+
+
+'''
+@dp.message_handler(content_types=[web_app_data])
 async def process_web_app_data(message: Message):
     if message.web_app_data == "send_order_info":
         order_data = json.loads(message.text)
@@ -67,8 +101,8 @@ async def send_order_to_user(user_id, order_data):
     await bot.send_message(user_id, f'Загальна ціна: {total_price} грн')
 
 
-@dp.message_handler(content_types='web_app_data')
-async def buy_process(queryId, web_app_message):
+@dp.message_handler(content_types=['web_app_data'])
+async def buy_process(web_app_message: Message):
     total_price = web_app_message.web_app_data.totalPrice
     await bot.send_invoice(web_app_message.chat.id, title='Замовлення', description='Ваше замовлення включає:', provider_token=PAYMENTS_TOKEN,
                            currency='grn', need_email=True, need_phone_number=True, prices=total_price,
@@ -81,6 +115,6 @@ async def checkout_process(pre_checkout_query: PreCheckoutQuery):
 @dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
 async def successful_payment(message: Message):
     await bot.send_message(message.chat.id, 'Платіж успішний')
-
+'''
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
