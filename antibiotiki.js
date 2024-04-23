42796e60177fb3bbcdf53683337a0e8a550de980
Maryna_Trush <149105658+Trush-Maryna@users.noreply.github.com>
2024-04-23 03:03:59 +0300

    Add files via upload

--- antibiotiki.js
+++ antibiotiki.js
@@ -5,6 +5,10 @@ tg.MainButton.color = "rgb(91,179,208)";
 tg.MainButton.fontSize = "17px";
 let selectedItems = [];
 
+document.addEventListener('DOMContentLoaded', function() {
+    updateMainButtonVisibility();
+});
+
 let items = document.querySelectorAll(".item");
 items.forEach(item => {
     let quantityText = item.querySelector(".quantity-text");
@@ -12,54 +16,54 @@ items.forEach(item => {
     let minusBtn = item.querySelector(".minus-btn");
     let addToCartBtn = item.querySelector(".add-to-cart-btn");
     let quantityContainer = item.querySelector(".quantity");
-    
+    let itemName = item.getAttribute("data-item");
+
     addToCartBtn.addEventListener("click", function() {
         quantityContainer.style.display = "inline-block";
         addToCartBtn.style.display = "none";
-        let itemName = item.getAttribute("data-item");
         let quantity = parseInt(quantityText.textContent);
         selectedItems.push({ name: itemName, quantity: quantity });
+        updateItemQuantity(itemName, quantity);
         updateMainButtonVisibility();
     });
-    
+
     plusBtn.addEventListener("click", function() {
         let quantity = parseInt(quantityText.textContent);
         if (!isNaN(quantity)) {
             quantity++;
             quantityText.textContent = quantity;
-            updateMainButtonVisibility();
-            let itemName = item.getAttribute("data-item");
             updateItemQuantity(itemName, quantity);
+            updateMainButtonVisibility();
         }
     });
-    
+
     minusBtn.addEventListener("click", function() {
         let quantity = parseInt(quantityText.textContent);
         if (!isNaN(quantity) && quantity > 0) {
             quantity--;
             quantityText.textContent = quantity;
-            updateMainButtonVisibility();
-            let itemName = item.getAttribute("data-item"); 
             updateItemQuantity(itemName, quantity);
+            updateMainButtonVisibility();
         }
     });
-
-    function updateItemQuantity(name, quantity) {
-        selectedItems.forEach(item => {
-            if (item.name === name) {
-                item.quantity = quantity;
-            }
-        });
-        localStorage.setItem("selectedItems", JSON.stringify(selectedItems));
-    }
-
-    function updateMainButtonVisibility() {
-        if (selectedItems.length > 0) {
-            tg.MainButton.setText(`Подивитися замовлення`);
-            tg.MainButton.show();
-        }
-    }
 });
+
+function updateItemQuantity(name, quantity) {
+    selectedItems.forEach(item => {
+        if (item.name === name) {
+            item.quantity = quantity;
+        }
+    });
+    localStorage.setItem("selectedItems", JSON.stringify(selectedItems));
+}
+
+function updateMainButtonVisibility() {
+    let cartItems = JSON.parse(localStorage.getItem('selectedItems')) || [];
+    if (cartItems.length > 0) {
+        tg.MainButton.setText(`Подивитися замовлення`);
+        tg.MainButton.show();
+    }
+}
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
     localStorage.setItem("selectedItems", JSON.stringify(selectedItems));
--- script_json.js
+++ script_json.js
@@ -2,63 +2,78 @@ document.addEventListener('DOMContentLoaded', function() {
     loadProducts();
 });
 
+window.addEventListener('popstate', function() {
+    loadProducts();
+});
+
 async function loadProducts() {
     try {
         const response = await fetch('https://trush-maryna.github.io/Maryna_Trush.github.io/products.json');
         const products = await response.json();
-
+        let startIndex, endIndex;
+        if (window.location.href.includes("antibiotiki.html") || window.location.href.includes("antibiotiki_menu.html")) {
+            startIndex = 0;
+            endIndex = 5;
+        } else if(window.location.href.includes("colds.html") || window.location.href.includes("colds_menu.html")){
+            startIndex = 6;
+            endIndex = 11;
+        }
         const productContainers = document.querySelectorAll('.item');
+        let productIndex = 0; 
         productContainers.forEach((productContainer, index) => {
-            const product = products[index];
-            const productsListDiv = productContainer.querySelector('.productsList');
-            const productDiv = document.createElement('div');
-            const productImage = document.createElement('img');
+            if (index >= startIndex && index <= endIndex && products[index]) { 
+                const product = products[index];
+                const productsListDiv = productContainer.querySelector('.productsList');
+                const productDiv = document.createElement('div');
+                const productImage = document.createElement('img');
 
-            productDiv.innerHTML = `<span class="productName">${product.name}</span> <br>
-                                    <span class="productDescr">${product.descr}</span> <br>
-                                    <span class="productPrice">${product.price} грн</span>`;
-            productImage.src = product.image;
+                productDiv.innerHTML = `<span class="productName">${product.name}</span> <br>
+                                        <span class="productDescr">${product.descr}</span> <br>
+                                        <span class="productPrice">${product.price} грн</span>`;
+                productImage.src = product.image;
 
-            productsListDiv.appendChild(productImage);
-            productsListDiv.appendChild(productDiv);
+                productsListDiv.appendChild(productImage);
+                productsListDiv.appendChild(productDiv);
 
-            const addToCartBtn = productContainer.querySelector('.add-to-cart-btn');
-            const quantityText = productContainer.querySelector('.quantity-text');
+                const addToCartBtn = productContainer.querySelector('.add-to-cart-btn');
+                const quantityText = productContainer.querySelector('.quantity-text');
 
-            addToCartBtn.addEventListener('click', function() {
-                const quantity = parseInt(quantityText.textContent);
-                const selectedItem = {
-                    name: product.name,
-                    descr: product.descr,
-                    price: product.price,
-                    image: product.image,
-                    quantity: quantity
-                };
-                addToCart(selectedItem);
-            });
+                addToCartBtn.addEventListener('click', function() {
+                    const quantity = parseInt(quantityText.textContent);
+                    const selectedItem = {
+                        name: product.name,
+                        descr: product.descr,
+                        price: product.price,
+                        image: product.image,
+                        quantity: quantity
+                    };
+                    addToCart(selectedItem);
+                });
 
-            const plusBtn = productContainer.querySelector('.plus-btn');
-            const minusBtn = productContainer.querySelector('.minus-btn');
+                const plusBtn = productContainer.querySelector('.plus-btn');
+                const minusBtn = productContainer.querySelector('.minus-btn');
 
-            let quantity = 1;
+                let quantity = 1;
 
-            plusBtn.addEventListener('click', function() {
-                quantity++;
-                quantityText.textContent = quantity;
-                updateMainButtonVisibility();
-                let itemName = product.name;
-                updateItemQuantity(itemName, quantity);
-            });
-
-            minusBtn.addEventListener('click', function() {
-                if (quantity > 1) {
-                    quantity--;
+                plusBtn.addEventListener('click', function() {
+                    quantity++;
                     quantityText.textContent = quantity;
                     updateMainButtonVisibility();
                     let itemName = product.name;
                     updateItemQuantity(itemName, quantity);
-                }
-            });
+                });
+
+                minusBtn.addEventListener('click', function() {
+                    if (quantity > 1) {
+                        quantity--;
+                        quantityText.textContent = quantity;
+                        updateMainButtonVisibility();
+                        let itemName = product.name;
+                        updateItemQuantity(itemName, quantity);
+                    }
+                });
+                productIndex++;
+            }
 
         });
     } catch (error) {
